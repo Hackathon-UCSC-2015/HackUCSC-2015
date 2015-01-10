2d563e3d01feb7fdba9bf73591321c7e8f4df0dc
pherniman <pherniman@gmail.com>
2015-01-09 16:55:17 -0800

    All that's needed are file io and proper commands written

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ var fs = require('fs');
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port:8080});
 
+var serverFunctions = { //functions for various commands
+};
+
 wss.on('connection', function(ws){
     console.log('user connected');
     ws.on('message', function(packet){
-        console.log("Received "+packet);
+        var decoded = JSON.parse(packet);
+        console.log("Received "+decoded); //debug
+        //search for and run the command recieved in our server table
+        var fn = serverFunctions[decoded.type];
+        fn && fn(decoded); //run the function if we find it in our table
     });
 });
+
