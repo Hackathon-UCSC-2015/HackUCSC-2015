aeb97612b99aa7619be61d777e2d5309122935fb
JaidenK <king.jaiden@yahoo.com>
2015-01-10 12:23:46 -0800

    pretty buttons

--- client/script.js
+++ client/script.js
@@ -131,4 +131,18 @@ $(document).ready(function() {
     $('#loginButton').click(function() {
         window.location.replace("/auth/google/");
     });
+	
+	$('#confirmButton').hover(function() {
+		$('#confirmText').show(200);
+	},function(){
+		$('#confirmText').hide(200);	
+	});
+	$('#confirmText').hide();
+	
+	$('#denyButton').hover(function() {
+		$('#denyText').show(200);
+	},function(){
+		$('#denyText').hide(200);	
+	});
+	$('#denyText').hide();
 });
