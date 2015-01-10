80239c90bf4f5e7e579cdd5df781ef1e71e6666f
Cole Faust <colecfaust@gmail.com>
2015-01-09 16:44:40 -0800

    added selecting events

--- client/script.js
+++ client/script.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
         console.log('Add event!');
         var newEvent = $('#eventTemplate').clone();
         newEvent.show();
+        newEvent.click(function() {
+            $('.event').removeClass('selected');
+            newEvent.toggleClass('selected');
+        });
         $('#eventList').prepend(newEvent);
     });
 });
