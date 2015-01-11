4308061ef39cb672edb271a48edbf20c6f7035d7
JaidenK <king.jaiden@yahoo.com>
2015-01-10 21:07:20 -0800

    Shows # people attending

--- client/script.js
+++ client/script.js
@@ -102,6 +102,11 @@ function displayEvent(eventID) {
     $('#editButton').hide();
     $('#startDate').val(data.startTime.toDateString());
     $('#endDate').val(data.endTime.toDateString());
+	if(data.attending.length==1){
+		$('#numberAttend').html("1 person attending");
+	}else{
+		$('#numberAttend').html(data.attending.length+" people attending");
+	}
     if(data.editing) {
         $('#eventDetails > h2').html(data.name).prop('contentEditable', true);
         $('#eventDetails > h2').get(0).addEventListener('input', headerCallback);
